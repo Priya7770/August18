babbd8a8db2bb0df53f7b31376316fb84880c3a9
Priya7770 <priyagoud196@gmail.com>
2021-08-18 15:27:26 +0530

    updatedsuccessfully

---
+++ src/com/git/Git_Commands.java
@@ -0,0 +1,23 @@
+package com.git;
+
+public class Git_Commands {
+	public static void main(String[] args) {
+		
+		System.out.println("Git commands");
+		System.out.println("Git GIThub");
+		System.out.println("Git push");
+		System.out.println("Git pull");
+		System.out.println("Git init");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
